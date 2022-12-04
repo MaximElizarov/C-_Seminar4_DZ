fbd9b4ca5c2f69992973d80bc05da9f554d5a0af
Maxim Elizmarov <kiro.dota322@gmail.com>
2022-12-04 18:12:48 +0300

    Add DZ

---
+++ Zadahya25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int n = a;
+for(int i = 1; i < b; i++)
+{    
+    n = n * a;   
+}
+Console.Write(n);
+
+
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// НЕ ИСПОЛЬЗОВАТЬ MATH.POW
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
---
+++ Zadahya29/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int n = 8;
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    array[i] = x;
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, предоставить возможность пользователю ввести числа и вывести их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
